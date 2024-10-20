0be3a18741b2fb05d4b409766903885d39aacc63
Dermot Logue <dermot.logue@dkit.ie>
2024-10-20 17:56:45 +0100

    updates

--- src/main/java/org/example/BetterRectangle.java
+++ src/main/java/org/example/BetterRectangle.java
@@ -1,15 +1,24 @@
 package org.example;
 
-// Exercise P9.10 - partial solution
+// Exercise P9.10 and P9.11 - partial solution
 //
 
+// Import the Rectangle class definition form the Java API
+// Look it up in the API documentation (or hover over the word Rectangle)
+// and you will see that this Rectangle hs a position (x,y) and width an height
+// fields.
 import java.awt.Rectangle;
+
 
 public class BetterRectangle extends Rectangle {
 
     public BetterRectangle(int x, int y, int width, int height) {
-        super(x, y, width, height);
-    }
+        super(x, y, width, height); // P9.11 -call the constructor in the Rectangle class
+
+//      setLocation(x,y);   // P9.10
+//      setSize(width,height);
+
+  }
 
     public double getArea() {
         return getWidth() * getHeight();
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,24 @@
 package org.example;
 
-// You must import this class from the Java Standard Library
-// Look up the class in the Java API
+// You must import this Rectangle class from the Java Standard Library.
+// Look up the class in the Java API to see its fields and methods.
 // You are going to extend this class (inherit from)
 // and add methods.
 
 import java.awt.Rectangle;
 
-// Partial solution (starter) to the P9.10 Exercise
+// Partial solution (starter) to the P9.10 ,9.11 Exercise
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         Rectangle r = new Rectangle(1,1,5,3);
         System.out.println(r);
 
         BetterRectangle betterRectangle = new BetterRectangle(2,2,5,7);
         System.out.println(betterRectangle.getArea());
 
-        // Complete the rest of the exercise
+        // Complete the rest of the exercise - write and call getPerimeter()
 
     }
 }
